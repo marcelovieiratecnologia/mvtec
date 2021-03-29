9804eb9e55c754b0a7f3f71b733e1ecc5a9171cc
marcelovieiratecnologia <marcelovieiratecnologia@gmail.com>
2021-03-29 15:51:42 -0300

    arquivos static

--- mvtec/settings.py
+++ mvtec/settings.py
@@ -29,10 +29,9 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-# ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=False, cast=Csv())
-ALLOWED_HOSTS =[]
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=False, cast=Csv())
 
 # Application definition
 
