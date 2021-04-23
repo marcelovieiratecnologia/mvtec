f45b4a1d9981bb0e54ea1449a814a7663865b11e
marcelovieiratecnologia <marcelovieiratecnologia@gmail.com>
2021-04-22 21:32:47 -0300

    colocando slide na home

--- app/home/views.py
+++ app/home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import Contact
-from ..blog.models import Category # usando para que eu deixe de usar os links no menu de modo FIXO e seja dinâmico
+from ..blog.models import Post, Category # usando para que eu deixe de usar os links no menu de modo FIXO e seja dinâmico
 from .forms import ContactForm
 
 
@@ -10,7 +10,8 @@ def home(request):
     # TODO: {'categorias_menu' em dois lugares , talvez melhor isso para não dar problema em dois lugares qdo atualizar alguma coisa
     #  .. na view home para qdo carregar da primeira vez e na VIEW BLOG para sem carregar a category.html qdo for chamada}
     categorias_menu = Category.objects.order_by('title') # transformando meu menu em dinâmico
-    return render(request, 'home/home.html', {'categorias_menu':categorias_menu})
+    blog_slide_random = Post.objects.order_by('?')[:4]
+    return render(request, 'home/home.html', {'categorias_menu':categorias_menu, 'blog_slide_random':blog_slide_random})
 
 def politica_privacidade(request):
     return render(request, 'home/politica_privacidade.html' )
